9cdbb8f3e99399a728a9dcd6d877a9f893dbd3fa
churloh <churloh@gmail.com>
2014-11-11 00:40:40 -0500

    Add some debugging code

--- Reporter.WebApi/ReporterWebApiModule.cs
+++ Reporter.WebApi/ReporterWebApiModule.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Linq;
+using System.Reflection;
 using Abp.Application.Services;
 using Abp.Modules;
 using Abp.WebApi;
@@ -16,6 +17,20 @@ namespace Reporter
         {
             //This code is used to register classes to dependency injection system for this assembly using conventions.
             IocManager.RegisterAssemblyByConvention(Assembly.GetExecutingAssembly());
+
+            var assembly = typeof(ReporterApplicationModule).Assembly;
+            foreach (var type in assembly.GetTypes())
+            {
+                if (!type.IsPublic)
+                    continue;
+                if (!type.IsInterface)
+                    continue;
+                if (!typeof(IApplicationService).IsAssignableFrom(type))
+                    continue;
+                if (!IocManager.IsRegistered(type))
+                    continue;
+                System.Diagnostics.Debug.Print(type.Name);
+            }
 
             //Creating dynamic Web Api Controllers for application services.
             //Thus, 'web api layer' is created automatically by ABP.
