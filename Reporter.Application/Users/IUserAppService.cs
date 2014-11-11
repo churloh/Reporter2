81fbdf3cfea7686fc509e365f4143dac09f519e5
churloh <churloh@gmail.com>
2014-11-10 22:34:20 -0500

    Fix user-service problems

---
+++ Reporter.Application/Users/IUserAppService.cs
@@ -0,0 +1,10 @@
+﻿using Abp.Application.Services;
+using Reporter.Users.Dtos;
+
+namespace Reporter.Users
+{
+    public interface IUserAppService : IApplicationService
+    {
+        GetAllUsersOutput GetAllUsers();
+    }
+}
---
+++ Reporter.Application/Users/UserAppService.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Abp.Domain.Repositories;
+using AutoMapper;
+using Reporter.Users.Dtos;
+
+namespace Reporter.Users
+{
+    public class UserAppService : ReporterAppServiceBase, IUserAppService
+    {
+        private readonly IRepository<User> userRepository;
+
+        // ABP provides that we can directly inject IRepository<User> (without creating any repository class)
+        public UserAppService(IRepository<User> userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
+        public GetAllUsersOutput GetAllUsers()
+        {
+            return new GetAllUsersOutput
+            {
+                Users = Mapper.Map<List<UserDto>>(userRepository.GetAllList())
+            };
+        }
+    }
+}
